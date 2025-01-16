9737fef9bdb58f9c0d944fb9115cbae5277424e1
Daniel Damasceno Meira <daniel.meira@campus.unimontes.int>
2025-01-16 14:40:51 -0300

    testing the model and db

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Models\Product;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/','home');
+Route::get('/', function(){
+    $products = Product::all();
+    echo '<pre>';
+    print_r($products->toArray());
+});
